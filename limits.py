73f765987c0cb23a1d633c8395fdeec8f07c6c28
mayurikpawar <59004485+mayurikpawar@users.noreply.github.com>
2020-12-30 20:59:53 +0530

    All file uploaded

---
+++ limits.py
@@ -0,0 +1,6 @@
+#Configuration for defining the datastore.
+
+MAX_KEY_LEN = 32  # characters
+MAX_VALUE_SIZE = 16 * 1024  # 16 Kbytes
+MAX_LOCAL_STORAGE_SIZE = 1 * 1024 * 1024 * 1024  # 1 GB
+LOCAL_STORAGE_PREPEND_PATH = "/tmp" 
